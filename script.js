bf93bf3d8fe48e58494a055c48cc31a6800cf1f3
Raphael Machado <raphael.mm.91@gmail.com>
2022-01-23 03:03:46 -0300

    calculator

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-const numbers = document.querySelectorAll("[data-number]")
+const buttons = Array.from(document.getElementsByClassName("buttons"));
+const output = document.querySelector("[data-output]");
+
+buttons.map(button => {
+    button.addEventListener('click', e => {
+        switch (e.target.innerText) {
+            case 'C':
+                output.textContent = "";
+                break;
+            case "🔙":
+                if(output.textContent){
+                output.textContent = output.textContent.slice(0, -1);
+            }
+            break;
+            case '÷':
+                output.textContent += '/'
+                break;
+            case '=':
+                try{
+                   output.textContent = eval(output.textContent); 
+                }
+                catch{
+                    output.textContent = 'Error!'
+                }
+                break;
+            default:
+                output.textContent += e.target.textContent;
+        }
+    })
+})
